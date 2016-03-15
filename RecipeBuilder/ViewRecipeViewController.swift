f4463ae4376daded86971367cd6b4b13e271473c
inajnaa <anjanib@gmail.com>
2016-03-14 20:07:34 -0700

    adding sections instead of rows

--- RecipeBuilder/ViewRecipeViewController.swift
+++ RecipeBuilder/ViewRecipeViewController.swift
@@ -22,7 +22,12 @@ class ViewRecipeViewController: UIViewController, UITableViewDataSource, UITable
     }
   
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if section == 0 {
+            return 5
+        }
+        else {
+            return 4
+        }
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -30,74 +35,68 @@ class ViewRecipeViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        if indexPath.row == 0 {
-            let recipeImageCell = tableView.dequeueReusableCellWithIdentifier("RecipeImageCell") as! RecipeImageCell
-            return recipeImageCell
+
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                let recipeImageCell = tableView.dequeueReusableCellWithIdentifier("RecipeImageCell") as! RecipeImageCell
+                return recipeImageCell
+            }
+            else if indexPath.row == 1 {
+                let categoryCell = tableView.dequeueReusableCellWithIdentifier("CategoryCell") as! CategoryCell
+                return categoryCell
+            }
+            else if indexPath.row == 2 {
+                let titleCell = tableView.dequeueReusableCellWithIdentifier("TitleCell") as! TitleCell
+                return titleCell
+            }
+            else if indexPath.row == 3 {
+                let descriptionCell = tableView.dequeueReusableCellWithIdentifier("DescriptionCell") as! DescriptionCell
+                return descriptionCell
+            }
+            else {
+                let detailsCell = tableView.dequeueReusableCellWithIdentifier("DetailsCell") as! DetailsCell
+                return detailsCell
+            }
         }
         
-        else if indexPath.row == 1 {
-            let categoryCell = tableView.dequeueReusableCellWithIdentifier("CategoryCell") as! CategoryCell
-            return categoryCell
-        }
-        
-        else if indexPath.row == 2  {
-            let titleCell = tableView.dequeueReusableCellWithIdentifier("TitleCell") as! TitleCell
-            return titleCell
-        }
-        
-        else if indexPath.row == 3 {
-            let descriptionCell = tableView.dequeueReusableCellWithIdentifier("DescriptionCell") as! DescriptionCell
-            return descriptionCell
-        }
-        
-        else if indexPath.row == 4 {
-            let detailsCell = tableView.dequeueReusableCellWithIdentifier("DetailsCell") as! DetailsCell
-            return detailsCell
-        }
-            
-        else if indexPath.row == 5 {
-            let ingredientsHeaderCell = tableView.dequeueReusableCellWithIdentifier("IngredientsHeaderCell") as! IngredientsHeaderCell
-            return ingredientsHeaderCell
-        }
-        
-        else if indexPath.row == 6 || indexPath.row == 7 || indexPath.row == 8 || indexPath.row == 9 {
+        else if indexPath.section == 1 {
             let ingredientsCell = tableView.dequeueReusableCellWithIdentifier("IngredientsCell") as! IngredientsCell
             return ingredientsCell
         }
         
-        else if indexPath.row == 10 {
-            let directionsHeaderCell = tableView.dequeueReusableCellWithIdentifier("DirectionsHeaderCell") as! DirectionsHeaderCell
-            return directionsHeaderCell
-        }
-            
-        else if indexPath.row == 11 || indexPath.row == 12 || indexPath.row == 13 {
-            let directionsCell = tableView.dequeueReusableCellWithIdentifier("DirectionsCell") as! DirectionsCell
-            return directionsCell
-        }
-            
         else {
             let directionsCell = tableView.dequeueReusableCellWithIdentifier("DirectionsCell") as! DirectionsCell
             return directionsCell
         }
+
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        if indexPath.row == 0 {
-            return 230
+        
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                return 230
+            }
+            else if indexPath.row == 1 {
+                return 30
+            }
+            else if indexPath.row == 2 {
+                return 40
+            }
+            else if indexPath.row == 3 {
+                return 50
+            }
+            else {
+                return 40
+            }
         }
-        else if indexPath.row == 2 || indexPath.row == 4 {
+            
+        else {
             return 40
         }
-        else if indexPath.row == 3 || indexPath.row == 5 || indexPath.row == 10 {
-            return 50
-        }
-        else {
-            return 30
-        }
-  
         
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

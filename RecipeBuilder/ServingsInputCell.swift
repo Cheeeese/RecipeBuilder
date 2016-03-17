cd9aefec5cc5bb4ce61da19e5525a214c366de1e
Dorahan Arapgirlioglu <dorahann@gmail.com>
2016-03-16 20:00:40 -0700

    Servings label cell

---
+++ RecipeBuilder/ServingsInputCell.swift
@@ -0,0 +1,26 @@
+//
+//  ServingsInputCell.swift
+//  RecipeBuilder
+//
+//  Created by Dorahan Arapgirlioglu on 3/16/16.
+//  Copyright © 2016 Cheeeese. All rights reserved.
+//
+
+import UIKit
+
+class ServingsInputCell: UITableViewCell {
+    
+    @IBOutlet weak var ServingsInputLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

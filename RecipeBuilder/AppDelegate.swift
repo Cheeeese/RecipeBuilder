a49afac010ad0b899708d677857632ccbe68478a
inajnaa <anjanib@gmail.com>
2016-03-20 20:46:00 -0700

    log in animations

--- RecipeBuilder/AppDelegate.swift
+++ RecipeBuilder/AppDelegate.swift
@@ -35,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //            
 //            window?.rootViewController = vc
 //        }
+   
         
         return true
     }
--- RecipeBuilder/LoginViewController.swift
+++ RecipeBuilder/LoginViewController.swift
@@ -13,12 +13,56 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
 
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var iconImage: UIView!
+    @IBOutlet weak var fieldsView: UIView!
+    @IBOutlet weak var signInLink: UIButton!
+    @IBOutlet weak var buttonsView: UIView!
+    
+    var initialY: CGFloat!
+    var offset: CGFloat!
+    var initialButtonsY: CGFloat!
+    var offsetButtons: CGFloat!
+    var initialIconImageY: CGFloat!
+    var offsetIcon: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        signUpButton.layer.cornerRadius = 4
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        initialY = fieldsView.frame.origin.y
+        offset = -55
+        initialButtonsY = buttonsView.frame.origin.y
+        offsetButtons = -90
+        initialIconImageY = iconImage.frame.origin.y
+        offsetIcon = -30
+        
     }
-
+    
+    @IBAction func didTapBG(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        fieldsView.frame.origin.y = initialY + offset
+        buttonsView.frame.origin.y = initialButtonsY + offsetButtons
+        iconImage.frame.origin.y = initialIconImageY + offsetIcon
+        
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        fieldsView.frame.origin.y = initialY
+        buttonsView.frame.origin.y = initialButtonsY
+        iconImage.frame.origin.y = initialIconImageY
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
